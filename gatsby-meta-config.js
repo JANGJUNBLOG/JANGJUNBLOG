cdf72022a39e55cae8617848aaba3cf1643dbab6
jangjunblog <jangjunblog@gmail.com>
2020-10-02 15:41:12 +0900

    add blog start

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `장준 Blog`,
+  description: `Blog posted about everything with 'JANGJUN' point of view`,
+  author: `장준`,
+  introduction: `독보적인 장준의 시각, 포스팅 장준`,
+  siteUrl: `https://locolhost:8000`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: ``, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `./content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'https://github.com/JANGJUNBLOG', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 4, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
---
+++ src/components/bio/index.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import { StaticQuery, graphql, Link } from 'gatsby'
+import Image from 'gatsby-image'
+
+import './index.scss'
+
+export const Bio = () => (
+  <StaticQuery
+    query={bioQuery}
+    render={data => {
+      const { author, social, introduction } = data.site.siteMetadata
+
+      return (
+        <div className="bio">
+          <div className="author">
+            <div className="author-description">
+              <Image
+                className="author-image"
+                fixed={data.avatar.childImageSharp.fixed}
+                alt={author}
+                style={{
+                  borderRadius: `100%`,
+                }}
+              />
+              <div className="author-name">
+                <span className="author-name-prefix">Written by</span>
+                <Link to={'/about'} className="author-name-content">
+                  <span>@{author}</span>
+                </Link>
+                <div className="author-introduction">{introduction}</div>
+                <p className="author-socials">
+                  {social.github && (
+                    <a href={`https://github.com/${social.github}`}>GitHub</a>
+                  )}
+                  {social.medium && (
+                    <a href={`https://medium.com/${social.medium}`}>Medium</a>
+                  )}
+                  {social.twitter && (
+                    <a href={`https://twitter.com/${social.twitter}`}>
+                      Twitter
+                    </a>
+                  )}
+                  {social.facebook && (
+                    <a href={`https://www.facebook.com/${social.facebook}`}>
+                      Facebook
+                    </a>
+                  )}
+                  {social.linkedin && (
+                    <a href={`https://www.linkedin.com/in/${social.linkedin}/`}>
+                      LinkedIn
+                    </a>
+                  )}
+                </p>
+              </div>
+              <div className="author-sign">
+                <Image
+                  className="sign-image"
+                  fixed={data.sign.childImageSharp.fixed}
+                  alt={author}
+                  style={{}}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }}
+  />
+)
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/JANG.jpg/" }) {
+      childImageSharp {
+        fixed(width: 72, height: 72) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    sign: file(absolutePath: { regex: "/jjsign.png/" }) {
+      childImageSharp {
+        fixed(width: 42, height: 41) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        introduction
+        social {
+          twitter
+          github
+          medium
+          facebook
+          linkedin
+        }
+      }
+    }
+  }
+`
+
+export default Bio
